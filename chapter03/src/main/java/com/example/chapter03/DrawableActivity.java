96f203ff514f355616088bf48799e74d3f57d0fa
icu-luan <342139103@qq.com>
2024-05-12 16:39:52 +0800

    状态列表图形,checkbox复选框,selector选择器

--- chapter03/src/main/java/com/example/chapter03/DrawableActivity.java
+++ chapter03/src/main/java/com/example/chapter03/DrawableActivity.java
@@ -3,12 +3,25 @@ package com.example.chapter03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
-public class DrawableActivity extends AppCompatActivity {
+public class DrawableActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawable);
+        CheckBox ck_system = findViewById(R.id.ck_system);
+        CheckBox ck_custom = findViewById(R.id.ck_custom);
+        ck_system.setOnCheckedChangeListener(this);
+        ck_custom.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        String desc = String.format("您s%了这个checkbox",isChecked? "勾选":"取消勾选");
+        buttonView.setText(desc);
     }
 }
