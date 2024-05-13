1b421f34d2ffaade9a52c6169a935244a555f93d
icu-luan <342139103@qq.com>
2024-05-13 17:02:38 +0800

    202405131701
    提醒对话框,日期对话框

--- chapter03/src/main/java/com/example/chapter03/DrawableActivity.java
+++ chapter03/src/main/java/com/example/chapter03/DrawableActivity.java
@@ -2,6 +2,9 @@ package com.example.chapter03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
@@ -10,6 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Switch;
@@ -19,12 +23,17 @@ import android.widget.Toast;
 import com.example.chapter03.utils.DateUtil;
 import com.example.chapter03.utils.ViewUtil;
 
-public class DrawableActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener, RadioGroup.OnCheckedChangeListener, View.OnFocusChangeListener {
+import java.util.Calendar;
+
+public class DrawableActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener, RadioGroup.OnCheckedChangeListener, View.OnFocusChangeListener, View.OnClickListener, DatePickerDialog.OnDateSetListener {
 
     private TextView sw_result;
     private TextView ck_sw_result;
     private TextView rb_result;
     private EditText et_phone;
+    private TextView tv_alert;
+    private DatePicker dp_date;
+    private TextView tv_date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +65,15 @@ public class DrawableActivity extends AppCompatActivity implements CompoundButto
         EditText et_password1 = findViewById(R.id.et_password1);
         et_phone1.addTextChangedListener(new HideTextWatcher(et_phone1,11));
         et_password1.addTextChangedListener(new HideTextWatcher(et_password1,6));
+        //提醒对话框
+        findViewById(R.id.btn_alert).setOnClickListener(this);
+        tv_alert = findViewById(R.id.tv_alert);
+        //日期对话框
+        findViewById(R.id.btn_ok).setOnClickListener(this);
+        findViewById(R.id.btn_date).setOnClickListener(this);
+        dp_date = findViewById(R.id.dp_date);
+        tv_date = findViewById(R.id.tv_date);
+
 
     }
 
@@ -101,6 +119,55 @@ public class DrawableActivity extends AppCompatActivity implements CompoundButto
                 Toast.makeText(this,"请输入11位手机号码",Toast.LENGTH_LONG).show();
             }
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_alert:
+                //创建提醒对话框构造器
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                //设置对话框的标题文本
+                builder.setTitle("尊敬的用户");
+                //设置对话框的文本内容
+                builder.setMessage("确认卸载?");
+                //设置对话框的肯定按钮文本及其点击监听器
+                builder.setPositiveButton("残忍卸载", (dialog, which) -> {
+                    tv_alert.setText("只能离开了");
+                });
+                //设置对话框的否定按钮文本及其点击监听器
+                builder.setNegativeButton("我在想想", (dialog, which) -> {
+                    tv_alert.setText("再陪一会儿");
+                });
+                //根据建造器构建提醒对话框对象
+                AlertDialog alertDialog = builder.create();
+                //显示提醒对话框
+                alertDialog.show();
+                break;
+            case R.id.btn_ok:
+                String desc =String.format("您选择的日期是%d年%d月%d日",dp_date.getYear(),dp_date.getMonth()+1,dp_date.getDayOfMonth());
+                tv_date.setText(desc);
+                break;
+            case R.id.btn_date:
+                //获取日历的一个实例
+//                Calendar calendar = Calendar.getInstance();
+//                calendar.get(Calendar.YEAR);
+//                calendar.get(Calendar.MONTH);
+//                calendar.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dialog = new DatePickerDialog(this, this, 2090, 5, 11);
+                dialog.show();
+                break;
+
+
+        }
+
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        String desc =String.format("您选择的日期是%d年%d月%d日",year,month+1,dayOfMonth);
+        tv_date.setText(desc);
     }
 
     private class HideTextWatcher implements TextWatcher {
