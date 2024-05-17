44b63936bca4037aef2d34299e80abf1417e7917
icu-luan <342139103@qq.com>
2024-05-17 15:10:40 +0800

    Update UserDBHelper.java
    版本号更新,添加字段方法onUpgrade()

--- chapter03/src/main/java/com/example/chapter03/database/UserDBHelper.java
+++ chapter03/src/main/java/com/example/chapter03/database/UserDBHelper.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.widget.LinearLayout;
+import android.util.Log;
 
 import com.example.chapter03.entity.User;
 import com.example.chapter03.utils.DateUtil;
@@ -16,8 +16,9 @@ import java.util.List;
 
 public class UserDBHelper extends SQLiteOpenHelper {
     private static final String DB_NAME = "user.db";
-    private static final int DB_VERSION = 1;
+    private static final int DB_VERSION = 2;
     private static final String TABLE_NAME = "user_info";
+    private static final String TAG = "cai";
     private static UserDBHelper mHelper = null;
     private SQLiteDatabase mRDB = null;
     private SQLiteDatabase mWDB = null;
@@ -27,36 +28,36 @@ public class UserDBHelper extends SQLiteOpenHelper {
     }
 
     //利用单例模式获取数据库帮助器的唯一实例
-    public static UserDBHelper getInstance(Context context){
-        if(mHelper == null){
+    public static UserDBHelper getInstance(Context context) {
+        if (mHelper == null) {
             mHelper = new UserDBHelper(context);
         }
         return mHelper;
     }
 
     //打开数据库的读连接
-    public SQLiteDatabase openReadLink(){
-        if (mRDB == null || !mRDB.isOpen()){
+    public SQLiteDatabase openReadLink() {
+        if (mRDB == null || !mRDB.isOpen()) {
             mRDB = mHelper.getReadableDatabase();
         }
         return mRDB;
     }
 
     //打开数据库的写连接
-    public SQLiteDatabase openWriteLink(){
-        if (mWDB == null || !mWDB.isOpen()){
+    public SQLiteDatabase openWriteLink() {
+        if (mWDB == null || !mWDB.isOpen()) {
             mWDB = mHelper.getWritableDatabase();
         }
         return mWDB;
     }
 
     //关闭数据库连接
-    public void closeLink(){
-        if (mRDB != null && mRDB.isOpen()){
+    public void closeLink() {
+        if (mRDB != null && mRDB.isOpen()) {
             mRDB.close();
             mRDB = null;
         }
-        if (mWDB != null && mWDB.isOpen()){
+        if (mWDB != null && mWDB.isOpen()) {
             mWDB.close();
             mWDB = null;
         }
@@ -65,7 +66,7 @@ public class UserDBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String sql = "CREATE TABLE IF NOT EXISTS " +TABLE_NAME+ " (" +
+        String sql = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (" +
                 "_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
                 "name VARCHAR NOT NULL," +
                 "age INTEGER NOT NULL," +
@@ -76,47 +77,59 @@ public class UserDBHelper extends SQLiteOpenHelper {
         db.execSQL(sql);
     }
 
+    //版本更新后,需要添加字段时使用
+    //修改版本号DB_VERSION后,会自动执行方法
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        Log.d(TAG, "onUpgrade oldVersion=" + oldVersion + ", newVersion=" +
+                newVersion);
+        if (newVersion > 1) {
+            //Android的ALTER命令不支持一次添加多列，只能分多次添加
+            String alter_sql = "ALTER TABLE " + TABLE_NAME + " ADD COLUMN phone VARCHAR;";
+            Log.d(TAG, "alter_sql:" + alter_sql);
+            db.execSQL(alter_sql);
+            alter_sql = "ALTER TABLE " + TABLE_NAME + " ADD COLUMN password VARCHAR;";
+            Log.d(TAG, "alter_sql:" + alter_sql);
+            db.execSQL(alter_sql); // 执行完整的SQL语句
+        }
     }
 
     //添加
-    public long insert(User user){
+    public long insert(User user) {
         ContentValues values = new ContentValues();
-        values.put("name",user.name);
-        values.put("age",user.age);
-        values.put("height",user.height);
-        values.put("weight",user.weight);
-        values.put("married",user.married);
+        values.put("name", user.name);
+        values.put("age", user.age);
+        values.put("height", user.height);
+        values.put("weight", user.weight);
+        values.put("married", user.married);
         values.put("update_time", DateUtil.getNowTime());
-        return mWDB.insert(TABLE_NAME,null,values);
+        return mWDB.insert(TABLE_NAME, null, values);
     }
 
     //删除
-    public long deleteByName(String name , String age){
+    public long deleteByName(String name, String age) {
         //删除所有
 //        mWDB.delete(TABLE_NAME,"1=1",null);
-        return mWDB.delete(TABLE_NAME,"name=? and age=?",new String[]{name,age});
+        return mWDB.delete(TABLE_NAME, "name=? and age=?", new String[]{name, age});
     }
 
     //修改
-    public long update(User user){
+    public long update(User user) {
         ContentValues values = new ContentValues();
-        values.put("name",user.name);
-        values.put("age",user.age);
-        values.put("height",user.height);
-        values.put("weight",user.weight);
-        values.put("married",user.married);
+        values.put("name", user.name);
+        values.put("age", user.age);
+        values.put("height", user.height);
+        values.put("weight", user.weight);
+        values.put("married", user.married);
         values.put("update_time", DateUtil.getNowTime());
-        return mWDB.update(TABLE_NAME,values,"name=?",new String[]{user.name});
+        return mWDB.update(TABLE_NAME, values, "name=?", new String[]{user.name});
     }
 
     //查询所有
-    public List<User> queryAll(){
+    public List<User> queryAll() {
         List<User> list = new ArrayList<>();
-        Cursor cursor = mRDB.query(TABLE_NAME,null,null,null,null,null,null);
-        while (cursor.moveToNext()){
+        Cursor cursor = mRDB.query(TABLE_NAME, null, null, null, null, null, null);
+        while (cursor.moveToNext()) {
             User user = new User();
             user.id = cursor.getInt(0);
             user.name = cursor.getString(1);
@@ -124,16 +137,17 @@ public class UserDBHelper extends SQLiteOpenHelper {
             user.height = cursor.getLong(3);
             user.weight = cursor.getFloat(4);
             //SQLite没有布尔型,用0代表false,1代表true
-            user.married = (cursor.getInt(5) == 0) ?false:true;
+            user.married = (cursor.getInt(5) == 0) ? false : true;
             list.add(user);
         }
         return list;
     }
+
     //条件查询
-    public List<User> queryByName(String name){
+    public List<User> queryByName(String name) {
         List<User> list = new ArrayList<>();
-        Cursor cursor = mRDB.query(TABLE_NAME,null,"name=?",new String[]{name},null,null,null);
-        while (cursor.moveToNext()){
+        Cursor cursor = mRDB.query(TABLE_NAME, null, "name=?", new String[]{name}, null, null, null);
+        while (cursor.moveToNext()) {
             User user = new User();
             user.id = cursor.getInt(0);
             user.name = cursor.getString(1);
@@ -141,7 +155,7 @@ public class UserDBHelper extends SQLiteOpenHelper {
             user.height = cursor.getLong(3);
             user.weight = cursor.getFloat(4);
             //SQLite没有布尔型,用0代表false,1代表true
-            user.married = (cursor.getInt(5) == 0) ?false:true;
+            user.married = (cursor.getInt(5) == 0) ? false : true;
             list.add(user);
         }
         return list;
